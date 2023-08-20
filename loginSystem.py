2a9518fa057226261cff4e8b1c637f818ebb1623
nadahmedx <142652539+nadahmedx@users.noreply.github.com>
2023-08-20 14:54:38 +0300

    Update loginSystem.py

--- loginSystem.py
+++ loginSystem.py
@@ -24,37 +24,6 @@ def register():
     secret = input("Now enter your secrete phrase for safekeeping: ")
     database[username] = {"password": new_password, "secret": secret}
     return 1
-def choice_printer():
-    print("\nWhat would you like to do?")
-    print("1)login ")
-    print("2)register ")
-    print("3) Exit\n")
-    choice = int(input("> "))
-    print("")
-    return choice
-while True: #loop runs forever till break
-    choice=choices_printer()
-    match choice:
-        case 1:
-
-            has_username = login()
-            if has_username == 0:
-                print("error: username not found")
-            elif has_username == -1:
-                print("error: incorrect password")
-            else:
-                print(f"\nWelcome back, {has_username}!")
-                print("Your secret phrase is:")
-                print(database[has_username]["secret"])
-            continue
-        case 2:
-            register=register()
-            if registered == 1:
-                print("\nSuccessfuly Registered!")
-            elif registered==-1:
-                print("error: username already exists")
-            continue
-        case 3:
             print("Thank You")
             break
         case _:
